aea8e6e2aa13a436284204a61ff373196b83fcca
Severuna <oxxxyfomenkova@gmail.com>
2023-05-01 15:43:57 +0300

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './components/Main';
+
+function Main ( ) {
+
+  const [contacts, setContacts] = useState([
+    {
+        firstName: "Барней",
+        lastName: "Стинсовський",
+        phone: "+380956319521",
+        gender: "male"
+    }, {
+        firstName: "Робін",
+        lastName: "Щербатська",
+        phone: "+380931460123",
+        gender: "female"
+    }, {
+        firstName: "Анонімний",
+        lastName: "Анонімус",
+        phone: "+380666666666"
+    }, {
+        firstName: "Лілія",
+        lastName: "Олдровна",
+        phone: "+380504691254",
+        gender: "female"
+    }, {
+        firstName: "Маршен",
+        lastName: "Еріксонян",
+        phone: "+380739432123",
+        gender: "male"
+    }, {
+        firstName: "Теодор",
+        lastName: "Мотсбес",
+        phone: "+380956319521",
+        gender: "male"
+    }
+  ]);
+
+  let elements = [];
+
+  contacts.forEach((el, i) => {
+    console.log(el.firstName)
+    elements.push(el.firstName)
+  });
+
+  console.log(elements)
+
+  return (
+      <div className='main column'>
+          <h1 className='title'>Homework 16. ReactJS. Hooks</h1>
+      </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
