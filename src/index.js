7f99440fecb08d5ead433600ed51d30d366404fa
Severuna <oxxxyfomenkova@gmail.com>
2023-05-01 16:24:53 +0300

    end refactoring code

--- src/index.js
+++ src/index.js
@@ -4,53 +4,60 @@ import './index.css';
 
 function Main ( ) {
 
-  const [contacts, setContacts] = useState([
-    {
-        firstName: "Барней",
-        lastName: "Стинсовський",
-        phone: "+380956319521",
-        gender: "male"
-    }, {
-        firstName: "Робін",
-        lastName: "Щербатська",
-        phone: "+380931460123",
-        gender: "female"
-    }, {
-        firstName: "Анонімний",
-        lastName: "Анонімус",
-        phone: "+380666666666"
-    }, {
-        firstName: "Лілія",
-        lastName: "Олдровна",
-        phone: "+380504691254",
-        gender: "female"
-    }, {
-        firstName: "Маршен",
-        lastName: "Еріксонян",
-        phone: "+380739432123",
-        gender: "male"
-    }, {
-        firstName: "Теодор",
-        lastName: "Мотсбес",
-        phone: "+380956319521",
-        gender: "male"
-    }
-  ]);
+      const [contacts, setContacts] = useState([
+        {
+            firstName: "Барней",
+            lastName: "Стинсовський",
+            phone: "+380956319521",
+            gender: "male"
+        }, {
+            firstName: "Робін",
+            lastName: "Щербатська",
+            phone: "+380931460123",
+            gender: "female"
+        }, {
+            firstName: "Анонімний",
+            lastName: "Анонімус",
+            phone: "+380666666666"
+        }, {
+            firstName: "Лілія",
+            lastName: "Олдровна",
+            phone: "+380504691254",
+            gender: "female"
+        }, {
+            firstName: "Маршен",
+            lastName: "Еріксонян",
+            phone: "+380739432123",
+            gender: "male"
+        }, {
+            firstName: "Теодор",
+            lastName: "Мотсбес",
+            phone: "+380956319521",
+            gender: "male"
+        }
+    ]);
 
-  let elements = [];
+    let elements = [];
 
-  contacts.forEach((el, i) => {
-    console.log(el.firstName)
-    elements.push(el.firstName)
-  });
-
-  console.log(elements)
+    contacts.forEach((el, i) => {
+        elements.push(
+        <div key={i} className='item row'>
+            <p className='item-field'><span>Contact:</span> {el.firstName} {el.lastName}</p>
+            <p className='item-field'><span>Phone:</span> {el.phone}</p>
+        </div>
+        )
+    });
 
   return (
-      <div className='main column'>
-          <h1 className='title'>Homework 16. ReactJS. Hooks</h1>
-      </div>
+    <div className='main column'>
+      <h1 className='title'>Homework 16.ReactJS.Hooks</h1>
+        <div className='container column'>
+          <h2 className='subtitle'>Contacts</h2>
+            {elements}
+        </div>
+    </div>
   )
+
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
