b14646c72ad33c09e76352e83855e135024b7949
Severuna <oxxxyfomenkova@gmail.com>
2023-05-12 19:09:15 +0300

    add routing

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {  Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const Main = () => {
     return (
         <div>
-        <h1 className='title'>Init title</h1>
+        <Routes>
+            <Route path='/react_styled/register' element={<Register />} />
+            <Route path='/react_styled/' element={<Login />} />
+        </Routes>
         </div>
     );
 };
