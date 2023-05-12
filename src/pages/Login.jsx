b030a8863c7156ee96d5e78413c2015fed627b80
Severuna <oxxxyfomenkova@gmail.com>
2023-05-12 22:39:49 +0300

    add icon padlock

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { FormCase } from '../styled-components/FormCase';
+import { Icon } from '../styled-components/Icon';
+import padlock from '../img/padlock.svg';
 
 const Login = () => {
     return (
         <FormCase>
+            <Icon src={padlock} alt='padlock' />
             <h1 className='title'>Login</h1>
         </FormCase>
     );
---
+++ src/styled-components/Icon.jsx
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const Icon = styled.img`
+    padding: 1rem;
+    border-radius: 50%;
+    background-color: pink;
+    width: 7rem;
+`;
