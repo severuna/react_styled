f6dfa07663a25a91ff2f572f3e7e9add07608480
Severuna <oxxxyfomenkova@gmail.com>
2023-05-12 18:48:24 +0300

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import Main from './Main';
 
@@ -7,5 +8,7 @@ import Main from './Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Main />
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
 );
